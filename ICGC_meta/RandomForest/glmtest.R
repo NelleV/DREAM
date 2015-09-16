6f90b1366b11fe8ebebcb57a151f037e2751b745
DeveauP <paul.deveau@polytechnique.edu>
2015-09-16 18:12:25 +0200

    Checked version of glmtest
    - Fuctional
    - can convert information to add truth to feature table
    - Example given in comments

--- ICGC_meta/RandomForest/glmtest.R
+++ ICGC_meta/RandomForest/glmtest.R
@@ -8,7 +8,7 @@ RForestLog = function(apprent,validat, varY,varX,nb_var,crit = "BIC",nsimul = 1)
 	nobs<-nrow(apprent)
 	y<-apprent[,varY]
 	formule<-as.formula(paste("y ~ ",paste(names(apprent[,varX]),collapse ="+")))
-	sature<-glm(formule,sata =apprent[,c(varX,varY)],family = binomial(link = "logit"))
+	sature<-glm(formule,data =apprent[,c(varX,varY)],family = binomial(link = "logit"))
 	coef <- matrix (0,length(coef(sature)),nsimul+2)
 	rownames(coef)<-names(coef(sature))
 	
@@ -20,7 +20,7 @@ RForestLog = function(apprent,validat, varY,varX,nb_var,crit = "BIC",nsimul = 1)
 		size<-length(varX) ##How many explicative features
 		s<-sort(sample(size,nb_var,replace=F))
 		predicteurs<-varX[s]
-
+		print(predicteurs)
 		s<-sample(nobs,nobs,replace = T)
 		#writing formula with all randomly chosen predictors
 
@@ -49,14 +49,15 @@ RForestLog = function(apprent,validat, varY,varX,nb_var,crit = "BIC",nsimul = 1)
 		}else{
 			moypred <- apply(predic[,1:i],1,mean)					
 		}
+
 		## calculating AUC
 		target <- validat[,varY]
 		pred <- prediction(moypred, target,label.ordering = c(0,1))
-		auc[i] <- performance(pred, "auc")@y.value[[1]]
+		auc[i] <- performance(pred, "auc")@y.values[[1]]
 
 		##keeping non-zero coef
 		index <- match(names(coef(selection)), names(coef(sature)))
-		coed[index,i] <- coef(selection)
+		coef[index,i] <- coef(selection)
 
 	}
 	##mean coef of aggregated models
@@ -66,3 +67,27 @@ RForestLog = function(apprent,validat, varY,varX,nb_var,crit = "BIC",nsimul = 1)
 	rf <- list(auc, coef)
 	return(rf)
 }
+
+add_truth_col<-function(feature,truth_bed){
+	truth_col<-numeric()	
+	for(chr in unique(feature$CHROM)){
+		print(chr)
+		spare_pos<-feature$END[feature$CHROM==chr]
+		spare_truth<-truth_bed[truth[,1]==chr,2]
+		truth_col<-c(truth_col,sapply(X= spare_pos, FUN= function(z) {
+			if(sum(z %in% spare_truth)>0){
+				return(1)
+			}
+			else{ return(0)}
+		}))
+	}
+	result<-cbind(feature,truth_col)
+	return(result)
+}
+
+###example below:
+#features<-read.table('/data/tmp/2015...',header= TRUE)
+#truth <- read.table('/data/tmp/.../Clustering.../...bed')
+#features_with_truth<-add_truth_col(features,truth)
+#Log<-RForestLog(apprent = features_with_truth[1:50000,],validat =  features_with_truth[50001:70001,],varY = "truth_col",varX = names(features_with_truth[,4:9]),nb_var=1, crit = "bic", nsimul = 2)
+
